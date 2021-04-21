7f58fc68e847b804406d10a4c16ae43fb08f16a9
Ravikiran Yadava <ravikiransjce.jobs@gmail.com>
2021-04-21 16:20:41 +0530

    Added post router for new user account registratioin

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -8,13 +8,13 @@ import {generateToken} from '../utils/utils.js'
 const userRouter = express.Router();
 
 userRouter.get('/', (req, res) => {
-    return res.send({users: data.users});
+  return res.send({users: data.users});
 })
 
 userRouter.get('/seed', expressAsyncHandler(async (req, res) => {
-    //await User.remove({});
-    const createdUsers = await User.insertMany(data.users);
-    return res.send({createdUsers})
+  //await User.remove({});
+  const createdUsers = await User.insertMany(data.users);
+  return res.send({createdUsers})
 }))
 
 userRouter.post('/signin', expressAsyncHandler(async(req, res) => {
@@ -22,19 +22,40 @@ userRouter.post('/signin', expressAsyncHandler(async(req, res) => {
     const user = await User.findOne({email: req.body.email});
 
     if(user){
-        if(bcrypt.compareSync(req.body.password, user.password)){
-            res.send({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                isAdmin: user.isAdmin,
-                token: generateToken(user)
-            });
-            return ;
-        }
+      if(bcrypt.compareSync(req.body.password, user.password)){
+        res.send({
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          isAdmin: user.isAdmin,
+          token: generateToken(user)
+        });
+        return ;
+      }
     }
-    res.status(401).send({message: 'Invalid user email or password'}); 
+    res.status(401).send({message: 'Invalid user email or password'});
 }))
 
+userRouter.post(
+  "/register",
+  expressAsyncHandler(async (req, res) => {
+      console.log(req.body);
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password, 8),
+    });
 
-export default userRouter
+    const createdUser = await newUser.save();
+
+    return res.send({
+      _id: createdUser._id,
+      name: createdUser.name,
+      email: createdUser.email,
+      isAdmin: false,
+      token: generateToken(createdUser),
+    });
+  })
+);
+
+export default userRouter;
